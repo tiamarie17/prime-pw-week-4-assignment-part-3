3c18e482151bc953cb05c019ed1cd43f697e2eac
Tia Theisen <tiatheisen@gmail.com>
2022-09-09 23:55:48 -0500

    added a function to add items to an array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,41 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+console.log(basket);
+
+function addItem(item) {
+    basket += item + '\n';
+    return true;   
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding soccer ball (expect true)', addItem('soccer ball '));
+console.log(`Basket is now ${basket}`);
+console.log('Adding cleats (expect true)', addItem('cleats'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding socks (expect true)', addItem('socks'));
+console.log(`Basket is now ${basket}`);
+
+
+function listItems(array){
+    for(let i=0; i < array.length; i++){
+        return array;
+    }
+}
+console.log('Listing items in basket array on a new line: \n' + listItems(basket));
+
+
+
+function empty(fullArray) {
+    fullArray = [];
+    return fullArray;
+   
+}
+console.log(empty(basket));
+
+
+
+
+
